1663ebf2ad9910ac809a389b4f5710fc07030d06
schibel19 <schibel19@up.edu>
2016-08-31 17:02:35 -0700

    fixed prime tester for real

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,7 +15,11 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n)
     {
-        // for now, return a random result
+        if(n == 0 || n == 1)
+        {
+            return false;
+        }
+
         for(int i=2; i < n ; i++)
         {
             if(n%i == 0)
@@ -23,6 +27,7 @@ public class PrimeTester {
                 return false;
             }
         }
+
         return true;
     }
 }
